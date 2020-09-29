145b23ded2495e005f5f839782a58c8bb54d4103
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-09-29 19:11:54 +0530

    Add storage service

---
+++ src/Timesheet/services/StorageService.js
@@ -0,0 +1,35 @@
+const DARK_MODE = "dark_mode";
+const LINES = "lines";
+
+export function getItem(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+
+export function setItem(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+export function getDarkMode() {
+  return getItem(DARK_MODE);
+}
+
+export function setDarkMode(value) {
+  return setItem(DARK_MODE, value);
+}
+
+export function setLines(value) {
+  return setItem(LINES, value);
+}
+
+export function getLines() {
+  return getItem(LINES);
+}
+
+export default {
+  getItem,
+  setItem,
+  getDarkMode,
+  setDarkMode,
+  setLines,
+  getLines,
+};
