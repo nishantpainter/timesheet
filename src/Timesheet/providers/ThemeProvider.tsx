0f4c9033851166f1631fee7200cfb81db567fab5
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-09-29 19:13:19 +0530

    Add theme provider

--- src/Timesheet/providers/ThemeProvider.tsx
+++ src/Timesheet/providers/ThemeProvider.tsx
@@ -5,19 +5,54 @@ import {
   createMuiTheme,
   Theme,
 } from "@material-ui/core/styles";
+import teal from "@material-ui/core/colors/teal";
+import blueGrey from "@material-ui/core/colors/blueGrey";
+
+import StorageService from "../services/StorageService";
+
+const ThemeContext = React.createContext({});
 
 type ThemeProviderProps = {};
 
-const theme: Theme = createMuiTheme({});
-
 const ThemeProvider: React.FC<ThemeProviderProps> = (props) => {
   const { children } = props;
+
+  const [darkTheme, setDarkTheme] = React.useState(
+    StorageService.getDarkMode()
+  );
+
+  const handleToggleDarkTheme = React.useCallback(() => {
+    setDarkTheme((darkTheme: Boolean) => {
+      StorageService.setDarkMode(!darkTheme);
+      return !darkTheme;
+    });
+  }, []);
+
+  const theme: Theme = createMuiTheme({
+    palette: {
+      type: darkTheme ? "dark" : "light",
+      primary: teal,
+      secondary: blueGrey,
+    },
+  });
+
+  const value = React.useMemo(
+    () => ({
+      handleToggleDarkTheme,
+    }),
+    [handleToggleDarkTheme]
+  );
+
   return (
-    <MuiThemeProvider theme={theme}>
-      <CssBaseline />
-      {children}
-    </MuiThemeProvider>
+    <ThemeContext.Provider value={value}>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </MuiThemeProvider>
+    </ThemeContext.Provider>
   );
 };
+
+export const useTheme = (): any => React.useContext(ThemeContext);
 
 export default ThemeProvider;
