16241a992ae0b46007c8ed2178a46453f1b8ea7c
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-09-29 19:12:50 +0530

    Add time sheet line

---
+++ src/Timesheet/components/TimesheetLine.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+
+import { Line } from "../types";
+import IconButton from "../components/IconButton";
+
+type TimesheetLineProps = {
+  line: Line;
+  onDelete: any;
+  onChange: any;
+};
+
+const TimesheetLine: React.FC<TimesheetLineProps> = (props) => {
+  const { line, onDelete, onChange } = props;
+
+  const handleDelete = React.useCallback(
+    (e) => {
+      onDelete(e, line);
+    },
+    [line, onDelete]
+  );
+
+  const handleChange = React.useCallback(
+    (e) => {
+      e.persist();
+      onChange(e, line);
+    },
+    [line, onChange]
+  );
+
+  return (
+    <Grid container spacing={1} alignItems="center">
+      <Grid item xs={7} sm={9}>
+        <TextField
+          variant="outlined"
+          size="small"
+          placeholder="Title"
+          name="title"
+          value={line.title}
+          onChange={handleChange}
+          fullWidth
+        />
+      </Grid>
+      <Grid item xs={4} sm={2}>
+        <TextField
+          variant="outlined"
+          size="small"
+          placeholder="Hours"
+          name="hours"
+          value={line.hours}
+          onChange={handleChange}
+          fullWidth
+        />
+      </Grid>
+      <Grid item xs={1} sm={1}>
+        <IconButton
+          icon="delete"
+          color="secondary"
+          onClick={handleDelete}
+        ></IconButton>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default TimesheetLine;
