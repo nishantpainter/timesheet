f8d8bb607ded9bb4461f40b73b5ce986ea955325
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-09-30 19:06:55 +0530

    Add imperative time configuration

--- src/Timesheet/providers/StoreProvider.tsx
+++ src/Timesheet/providers/StoreProvider.tsx
@@ -6,10 +6,12 @@ import StorageService from "../services/StorageService";
 
 type StoreContextType = {
   lines: Line[];
+  imperative: boolean;
   handleChangeLine: any;
   handleDeleteLine: any;
   handleDeleteAllLines: any;
   handleAddNewLine: any;
+  handleChangeImperative: any;
 };
 
 const defaultLines: Line[] = [];
@@ -19,6 +21,9 @@ const StoreContext = React.createContext<Partial<StoreContextType>>({});
 const StoreProvider = (props: any) => {
   const { children } = props;
   const [lines, setLines] = React.useState(defaultLines);
+  const [imperative, setImperative] = React.useState(
+    StorageService.getImperative()
+  );
 
   const fetchLines = () => {
     const lines = StorageService.getLines();
@@ -40,19 +45,32 @@ const StoreProvider = (props: any) => {
     setLines((lines) => lines.filter((l) => l.id !== line.id));
   }, []);
 
-  const handleChangeLine = React.useCallback((event, line) => {
-    const { name, value } = event.target;
+  const handleChangeLine = React.useCallback(
+    (event, line) => {
+      const { name, value } = event.target;
 
-    const declarativeTimeRegex = /^\d*\.?[0-5]?((?<=[0-5])[0-9]?)?$/;
+      const declarativeTimeRegex = /^\d*\.?[0-5]?((?<=[0-5])[0-9]?)?$/;
+      const imperativeTimeRegex = /^\d*\.?\d*$/;
 
-    if (name === "hours" && !declarativeTimeRegex.test(value)) {
-      return;
-    }
-    setLines((lines) => {
-      return lines.map((l) =>
-        l.id === line.id ? { ...line, [name]: value } : l
-      );
-    });
+      const timeRegex = imperative ? imperativeTimeRegex : declarativeTimeRegex;
+
+      if (name === "hours" && !timeRegex.test(value)) {
+        return;
+      }
+
+      setLines((lines) => {
+        return lines.map((l) =>
+          l.id === line.id ? { ...line, [name]: value } : l
+        );
+      });
+    },
+    [imperative]
+  );
+
+  const handleChangeImperative = React.useCallback((event) => {
+    const { checked } = event.target;
+    setImperative(checked);
+    StorageService.setImperative(checked);
   }, []);
 
   React.useEffect(() => fetchLines(), []);
@@ -60,17 +78,21 @@ const StoreProvider = (props: any) => {
   const value = React.useMemo(
     () => ({
       lines,
+      imperative,
       handleChangeLine,
       handleDeleteLine,
       handleDeleteAllLines,
       handleAddNewLine,
+      handleChangeImperative,
     }),
     [
       lines,
+      imperative,
       handleChangeLine,
       handleDeleteLine,
       handleDeleteAllLines,
       handleAddNewLine,
+      handleChangeImperative,
     ]
   );
 
