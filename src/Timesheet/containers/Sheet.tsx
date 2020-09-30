65ab90f4d2b1fb94e21d9a97b131cbf8b3db15e1
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-09-30 10:11:34 +0530

    Migrate store to provider

--- src/Timesheet/containers/Sheet.tsx
+++ src/Timesheet/containers/Sheet.tsx
@@ -1,17 +1,14 @@
 import React from "react";
-import { v4 as uuid } from "uuid";
+
 import { makeStyles } from "@material-ui/core";
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
-import { Line } from "../types";
-import StorageService from "../services/StorageService";
+import { useStore } from "../providers/StoreProvider";
 import TimesheetLineList from "../components/TimesheetLineList";
 
 type ListProps = {};
-
-const defaultLines: Line[] = [];
 
 const useStyles = makeStyles((theme) => ({
   button: { marginRight: theme.spacing() },
@@ -20,44 +17,13 @@ const useStyles = makeStyles((theme) => ({
 const List: React.FC<ListProps> = (props) => {
   const classes = useStyles();
 
-  const [lines, setLines] = React.useState(defaultLines);
-
-  const fetchLines = () => {
-    const lines = StorageService.getLines();
-    if (lines && lines.length) {
-      setLines(lines);
-    }
-  };
-
-  const handleAddNewLine = React.useCallback(() => {
-    setLines((lines: Line[]) => [
-      ...lines,
-      { id: uuid(), title: "", hours: "" },
-    ]);
-  }, []);
-
-  const handleDeleteAllLines = React.useCallback(() => {}, []);
-
-  const handleDeleteLine = React.useCallback((event, line) => {
-    setLines((lines) => lines.filter((l) => l.id !== line.id));
-  }, []);
-
-  const handleChangeLine = React.useCallback((event, line) => {
-    const { name, value } = event.target;
-
-    const declarativeTimeRegex = /^\d*\.?[0-5]?((?<=[0-5])[0-9]?)?$/;
-
-    if (name === "hours" && !declarativeTimeRegex.test(value)) {
-      return;
-    }
-    setLines((lines) => {
-      return lines.map((l) =>
-        l.id === line.id ? { ...line, [name]: value } : l
-      );
-    });
-  }, []);
-
-  React.useEffect(() => fetchLines(), []);
+  const {
+    lines = [],
+    handleAddNewLine,
+    handleChangeLine,
+    handleDeleteLine,
+    handleDeleteAllLines,
+  } = useStore();
 
   const totalHours = lines.reduce(
     (totalHours, line) => totalHours + Number(line.hours || 0),
--- src/Timesheet/providers/StoreProvider.tsx
+++ src/Timesheet/providers/StoreProvider.tsx
@@ -1,11 +1,78 @@
 import React from "react";
+import { v4 as uuid } from "uuid";
 
-const StoreContext = React.createContext({});
+import { Line } from "../types";
+import StorageService from "../services/StorageService";
+
+type StoreContextType = {
+  lines: Line[];
+  handleChangeLine: any;
+  handleDeleteLine: any;
+  handleDeleteAllLines: any;
+  handleAddNewLine: any;
+};
+
+const defaultLines: Line[] = [];
+
+const StoreContext = React.createContext<Partial<StoreContextType>>({});
 
 const StoreProvider = (props: any) => {
   const { children } = props;
+  const [lines, setLines] = React.useState(defaultLines);
 
-  const value = React.useMemo(() => ({}), []);
+  const fetchLines = () => {
+    const lines = StorageService.getLines();
+    if (lines && lines.length) {
+      setLines(lines);
+    }
+  };
+
+  const handleAddNewLine = React.useCallback(() => {
+    setLines((lines: Line[]) => [
+      ...lines,
+      { id: uuid(), title: "", hours: "" },
+    ]);
+  }, []);
+
+  const handleDeleteAllLines = React.useCallback(() => {}, []);
+
+  const handleDeleteLine = React.useCallback((event, line) => {
+    setLines((lines) => lines.filter((l) => l.id !== line.id));
+  }, []);
+
+  const handleChangeLine = React.useCallback((event, line) => {
+    const { name, value } = event.target;
+
+    const declarativeTimeRegex = /^\d*\.?[0-5]?((?<=[0-5])[0-9]?)?$/;
+
+    if (name === "hours" && !declarativeTimeRegex.test(value)) {
+      return;
+    }
+    setLines((lines) => {
+      return lines.map((l) =>
+        l.id === line.id ? { ...line, [name]: value } : l
+      );
+    });
+  }, []);
+
+  React.useEffect(() => fetchLines(), []);
+
+  const value = React.useMemo(
+    () => ({
+      lines,
+      handleChangeLine,
+      handleDeleteLine,
+      handleDeleteAllLines,
+      handleAddNewLine,
+    }),
+    [
+      lines,
+      handleChangeLine,
+      handleDeleteLine,
+      handleDeleteAllLines,
+      handleAddNewLine,
+    ]
+  );
 
   return (
     <StoreContext.Provider value={value}>{children}</StoreContext.Provider>
