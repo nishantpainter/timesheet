9a1fd7f7017f8ea412fc726b6978b97d839d8886
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-09-30 19:17:27 +0530

    Fix typo

--- src/Timesheet/components/ImperativeTimeInfo.tsx
+++ src/Timesheet/components/ImperativeTimeInfo.tsx
@@ -8,7 +8,7 @@ type ImperativeTimeInfoProps = {};
 const ImperativeTimeInfo: React.FC<ImperativeTimeInfoProps> = (props) => {
   return (
     <>
-      <Tooltip title="1 hour = 1.00; 30 mins = 0.50; 15 mins = 0.25">
+      <Tooltip title="1 hour = 1.00; 30 mins = 0.50; 15 mins = 0.25;">
         <IconButton icon="question"></IconButton>
       </Tooltip>
     </>
