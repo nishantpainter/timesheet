6c4da624330c0d894b8b212185e400f181d679a7
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-09-30 09:49:02 +0530

    Configure store provider

--- src/Timesheet/index.tsx
+++ src/Timesheet/index.tsx
@@ -3,14 +3,17 @@ import React from "react";
 import Content from "./containers/Content";
 import Toolbar from "./containers/Toolbar";
 import ThemeProvider from "./providers/ThemeProvider";
+import StoreProvider from "./providers/StoreProvider";
 
 type TimeSheetProps = {};
 
 const Timesheet: React.FC<TimeSheetProps> = () => {
   return (
     <ThemeProvider>
-      <Toolbar />
-      <Content />
+      <StoreProvider>
+        <Toolbar />
+        <Content />
+      </StoreProvider>
     </ThemeProvider>
   );
 };
