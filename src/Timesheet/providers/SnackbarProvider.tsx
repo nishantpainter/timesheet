e26867eb6bf7b14a468ec87e9762ad04d30a4b80
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-10-01 09:50:58 +0530

    Add dismiss action

--- src/Timesheet/providers/SnackbarProvider.tsx
+++ src/Timesheet/providers/SnackbarProvider.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
 import { SnackbarProvider as NotiStackProvider } from "notistack";
 
 type SnackbarProviderProps = {};
@@ -6,13 +7,25 @@ type SnackbarProviderProps = {};
 const SnackbarProvider: React.FC<SnackbarProviderProps> = (props) => {
   const { children } = props;
 
+  const notistackRef: any = React.createRef();
+
+  const onClickDismiss = (key: any) => () => {
+    notistackRef.current.closeSnackbar(key);
+  };
+
   return (
     <NotiStackProvider
       maxSnack={3}
+      ref={notistackRef}
       anchorOrigin={{
         vertical: "bottom",
         horizontal: "left",
       }}
+      action={(key) => (
+        <Button color="inherit" size="small" onClick={onClickDismiss(key)}>
+          Dismiss
+        </Button>
+      )}
     >
       {children}
     </NotiStackProvider>
