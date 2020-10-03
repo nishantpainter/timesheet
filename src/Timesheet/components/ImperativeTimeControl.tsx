c71c33429e0b005412cb825c1f2faf47f4cedb8c
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-10-03 17:49:40 +0530

    Extract imperative time control

---
+++ src/Timesheet/components/ImperativeTimeControl.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Box from "@material-ui/core/Box";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { makeStyles } from "@material-ui/core";
+
+import ImperativeTimeInfo from "./ImperativeTimeInfo";
+
+const useStyles = makeStyles((theme) => ({
+  imperativeFormControl: {
+    marginRight: theme.spacing(0.5),
+  },
+}));
+
+type ImperativeTimeControlProps = {
+  checked: boolean | undefined;
+  onChange: any;
+};
+
+const ImperativeTimeControl: React.FC<ImperativeTimeControlProps> = (props) => {
+  const { checked, onChange } = props;
+
+  const classes = useStyles();
+  return (
+    <Box display="flex" alignItems="center" height={40} marginBottom={1}>
+      <FormControlLabel
+        control={
+          <Checkbox checked={checked} onChange={onChange} color="primary" />
+        }
+        classes={{ root: classes.imperativeFormControl }}
+        label="Imperative Time"
+      />
+      &nbsp;
+      <ImperativeTimeInfo />
+    </Box>
+  );
+};
+
+export default ImperativeTimeControl;
--- src/Timesheet/containers/Sheet.tsx
+++ src/Timesheet/containers/Sheet.tsx
@@ -1,10 +1,7 @@
 import React from "react";
-
 import { makeStyles } from "@material-ui/core";
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
-import Checkbox from "@material-ui/core/Checkbox";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Typography from "@material-ui/core/Typography";
 import Dialog from "@material-ui/core/Dialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
@@ -13,9 +10,7 @@ import DialogActions from "@material-ui/core/DialogActions";
 
 import { useStore } from "../providers/StoreProvider";
 import TimesheetLineList from "../components/TimesheetLineList";
-import ImperativeTimeInfo from "../components/ImperativeTimeInfo";
-
-type ListProps = {};
+import ImperativeTimeControl from "../components/ImperativeTimeControl";
 
 const useStyles = makeStyles((theme) => ({
   button: { marginRight: theme.spacing() },
@@ -23,6 +18,8 @@ const useStyles = makeStyles((theme) => ({
     marginRight: theme.spacing(0.5),
   },
 }));
+
+type ListProps = {};
 
 const List: React.FC<ListProps> = (props) => {
   const classes = useStyles();
@@ -87,21 +84,10 @@ const List: React.FC<ListProps> = (props) => {
           Total Hours : <b>{totalHours.toFixed(2)}</b>
         </Typography>
       </Box>
-      <Box display="flex" alignItems="center" height={40} marginBottom={1}>
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={imperative}
-              onChange={handleChangeImperative}
-              color="primary"
-            />
-          }
-          classes={{ root: classes.imperativeFormControl }}
-          label="Imperative Time"
-        />
-        &nbsp;
-        <ImperativeTimeInfo />
-      </Box>
+      <ImperativeTimeControl
+        checked={imperative}
+        onChange={handleChangeImperative}
+      />
       <TimesheetLineList
         lines={lines}
         onDelete={handleDeleteLine}
