3a34834e5f07a0358c8a9ada32e6beecc9c2f9a9
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-10-04 13:12:25 +0530

    Update styles

--- src/Timesheet/containers/Content.tsx
+++ src/Timesheet/containers/Content.tsx
@@ -13,7 +13,7 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: theme.spacing(70),
     marginRight: "auto",
     marginLeft: "auto",
-    height: `calc(100% - ${theme.spacing(13.5)}px)`,
+    height: `calc(100% - ${theme.spacing(14.5)}px)`,
   },
 }));
 
