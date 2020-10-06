a35ab94467ecf5811b79e61201aa8a804aec8cab
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-10-06 19:11:18 +0530

    Update brightness icon

--- src/Timesheet/components/IconButton.tsx
+++ src/Timesheet/components/IconButton.tsx
@@ -4,7 +4,7 @@ import MuiIconButton, {
 } from "@material-ui/core/IconButton";
 
 import InfoIcon from "@material-ui/icons/Info";
-import BrightnessIcon from "@material-ui/icons/Brightness6";
+import BrightnessIcon from "@material-ui/icons/Brightness4";
 import SaveAltIcon from "@material-ui/icons/SaveAlt";
 import DeleteIcon from "@material-ui/icons/Delete";
 import HelpIcon from "@material-ui/icons/Help";
