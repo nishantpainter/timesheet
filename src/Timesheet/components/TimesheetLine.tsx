9021daf9470ac3165aed42d7e3d747a2c7a1422e
Nishant Painter <virtual.nishantpainter@gmail.com>
2020-10-07 19:09:48 +0530

    Align delete button

--- src/Timesheet/components/TimesheetLine.tsx
+++ src/Timesheet/components/TimesheetLine.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
 
 import { Line } from "../types";
 import IconButton from "../components/IconButton";
+
+const useStyles = makeStyles((theme) => ({
+  deleteButton: {
+    marginTop: theme.spacing(0.5),
+  },
+}));
 
 type TimesheetLineProps = {
   line: Line;
@@ -14,6 +21,8 @@ type TimesheetLineProps = {
 const TimesheetLine: React.FC<TimesheetLineProps> = (props) => {
   const { line, onDelete, onChange } = props;
 
+  const classes = useStyles();
+  
   const handleDelete = React.useCallback(
     (e) => {
       onDelete(e, line);
@@ -58,6 +67,7 @@ const TimesheetLine: React.FC<TimesheetLineProps> = (props) => {
         <IconButton
           icon="delete"
           color="secondary"
+          className={classes.deleteButton}
           onClick={handleDelete}
         ></IconButton>
       </Grid>
